59b0c12c454e76bee7b190df5747ccc78bb2360a
ericsonmoreira <moreira.ericson@gmail.com>
2021-01-20 23:52:10 -0300

    adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { ChakraProvider, Flex, Grid, Heading, Input } from '@chakra-ui/react';
 import * as React from 'react';
-import { ChakraProvider, Box, Text, theme } from '@chakra-ui/react';
+import custonTheme from './theme';
 
 export const App = () => (
-	<ChakraProvider theme={theme}>
-		<Box textAlign="center" fontSize="xl">
-			<Text>Testando</Text>
-		</Box>
+	<ChakraProvider theme={custonTheme}>
+		<Grid
+			height="100vh"
+			as="main"
+			templateColumns="1fr 300px 300px  1fr"
+			templateRows="1fr 300px 1fr"
+			templateAreas="
+        '. . . .'
+        '. logo form .'
+        '. . . .'"
+		>
+			<Flex gridArea="logo" flexDir="column" alignItems="flex-start">
+				<Heading size="2xl">Logo vai aqui</Heading>
+			</Flex>
+
+			<Flex
+				gridArea="form"
+				flexDir="column"
+				alignItems="stretch"
+				padding={16}
+				borderRadius="md"
+				backgroundColor="gray.700"
+			>
+				<Input height="50px" />
+			</Flex>
+		</Grid>
 	</ChakraProvider>
 );
---
+++ src/theme/index.ts
@@ -0,0 +1,6 @@
+import { extendTheme } from '@chakra-ui/react';
+
+// TODO: customizar aqui o thema
+const custonTheme = extendTheme({});
+
+export default custonTheme;
