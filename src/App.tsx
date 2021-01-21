da3b9e739206b4974a369e8db1b35271e24d4475
ericsonmoreira <moreira.ericson@gmail.com>
2021-01-20 22:41:36 -0300

    linter adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import * as React from "react";
-import { ChakraProvider, Box, Text, Code, theme } from "@chakra-ui/react";
+import * as React from 'react';
+import { ChakraProvider, Box, Text, theme } from '@chakra-ui/react';
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Text>
-        Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-      </Text>
-    </Box>
-  </ChakraProvider>
+	<ChakraProvider theme={theme}>
+		<Box textAlign="center" fontSize="xl">
+			<Text>Testando</Text>
+		</Box>
+	</ChakraProvider>
 );
