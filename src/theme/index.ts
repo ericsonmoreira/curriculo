e4eb5606b75eaa047d75c988602be386260e752a
ericsonmoreira <moreira.ericson@gmail.com>
2021-01-28 18:47:44 -0300

    add font Roboto

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,11 @@
 import { extendTheme } from '@chakra-ui/react';
 
 // TODO: customizar aqui o thema
-const custonTheme = extendTheme({});
+const custonTheme = extendTheme({
+	fonts: {
+		body: 'Roboto, sans-serif',
+    heading: 'Roboto, sans-serif',
+  },
+});
 
 export default custonTheme;
